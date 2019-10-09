7c6838eb3ca27b90cd506d06f8c0b8a4051c1928
Gina Graci <ginagraci92@gmail.com>
2019-10-09 00:10:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,3 +16,5 @@ if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
   puts "NO, NOT SINCE 1938!"
+else 
+  puts "HUH?! SPEAK UP, SONNY!"
