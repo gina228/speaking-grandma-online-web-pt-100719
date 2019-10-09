27f295e8c7df002dddb5c23de656717f9d547965
Gina Graci <ginagraci92@gmail.com>
2019-10-09 00:14:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(string)
   if string != string.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
 end
